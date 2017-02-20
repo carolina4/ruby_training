cb233b3fc80accae4fa366e87788c7b57e755980
Carolina Valerio <caro.cvs@gmail.com>
2017-02-20 17:04:53 -0600

    Started new note functionality.

--- notesapp/app/controllers/notes_controller.rb
+++ notesapp/app/controllers/notes_controller.rb
@@ -1,5 +1,19 @@
 class NotesController < ApplicationController
   def index
-    @notes = Note.all
+    @notes = Note.all.order(:updated_at)
+  end
+
+  # GET /resumes/new
+  def new
+    @note = Note.new
+  end
+
+  # GET /resumes/1/edit
+  def edit
+    @note = Resume.find(params[:id])
+  end
+
+  # POST /resumes
+  def create
   end
 end
