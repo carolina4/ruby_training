97d69f92439fdf91fbfcfd90b99d8e6f9a72490d
Carolina Valerio <caro.cvs@gmail.com>
2017-02-21 13:37:06 -0600

    Finished add and edit methods for note.

--- notesapp/app/controllers/notes_controller.rb
+++ notesapp/app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 class NotesController < ApplicationController
   def index
-    @notes = Note.all.order(:updated_at)
+    @notes = Note.all.order(updated_at: :desc)
   end
 
   # GET /resumes/new
@@ -8,12 +8,39 @@ class NotesController < ApplicationController
     @note = Note.new
   end
 
+  # GET /resumes/1
+  def show
+    note = Note.find(params[:id])
+    redirect_to edit_note_path(note)
+  end
+
   # GET /resumes/1/edit
   def edit
-    @note = Resume.find(params[:id])
+    @note = Note.find(params[:id])
   end
 
   # POST /resumes
   def create
+    note_params = params.require(:note).permit(:title, :note)
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to notes_path, notice: 'Note was successfully created.'
+    else
+      render :new
+    end
   end
+
+  # PATCH/PUT /resumes/1
+  def update
+    note_params = params.require(:note).permit(:title, :note)
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to notes_path, notice: 'Note was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
 end
