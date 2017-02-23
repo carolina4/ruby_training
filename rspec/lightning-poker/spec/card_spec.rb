9679c144738eb8cee94a5002215d274cdf892cd8
Carolina <caro.cvs@gmail.com>
2017-02-22 18:29:20 -0600

    testing ruby applications 2

--- rspec/lightning-poker/spec/card_spec.rb
+++ rspec/lightning-poker/spec/card_spec.rb
@@ -1,18 +1,77 @@
+require "set"
 require "card"
 
-RSpec.describe 'a playing card' do
+describe Card do
+	def card(params = {})
+		defaults = {
+			suit: :hearts,
+			rank: 7
+		}
+
+		Card.build(*defaults.merge(params).values_at(:suit, :rank))
+	end
+
 	it 'has a suit' do
-		raise unless Card.new(suit: :spades, rank: 4).suit == :spades
+		raise unless card(suit: :spades).suit == :spades
 	end
 
 	it 'has a rank' do
-		raise unless Card.new(suit: :spades, rank: 4).rank == 4
+		raise unless card(rank: 4).rank == 4
+	end
+
+	context 'equality' do
+		it 'is equal to itself' do
+			subject = card(suit: :spades, rank: 4)
+			other   = card(suit: :spades, rank: 4)
+
+			raise unless subject == other
+		end
+
+		it 'is hash equal to itself' do
+			subject = card(suit: :spades, rank: 4)
+			other   = card(suit: :spades, rank: 4)
+
+			raise unless Set.new([subject, other]).size == 1
+		end
+
+		describe 'comparing to a card of a different suit' do
+			it 'is not equal' do
+				subject = card(suit: :spades, rank: 4)
+				other   = card(suit: :hearts, rank: 4)
+
+				raise unless subject != other
+			end
+
+			it 'is not hash equal' do
+				subject = card(suit: :spades, rank: 4)
+				other   = card(suit: :hearts, rank: 4)
+
+				raise unless Set.new([subject, other]).size == 2
+			end
+		end
+
+		describe 'comparing to a card of a different rank' do
+			it 'is not equal' do
+				subject = card(suit: :spades, rank: 4)
+				other   = card(suit: :spades, rank: 5)
+
+				raise unless subject != other
+			end
+
+
+			it 'is not hash equal' do
+				subject = card(suit: :spades, rank: 4)
+				other   = card(suit: :spades, rank: 5)
+
+				raise unless Set.new([subject, other]).size == 2
+			end
+		end
 	end
 
 	describe 'a jack' do
 		it 'ranks higher than a 10' do
-			lower = Card.new(suit: :spades, rank: 10)
-			higher = Card.new(suit: :spades, rank: :jack)
+			lower = card(rank: 10)
+			higher = card(rank: :jack)
 
 			raise unless higher.rank > lower.rank
 		end
@@ -20,8 +79,8 @@ RSpec.describe 'a playing card' do
 
 	describe 'a queen' do
 		it 'ranks higher than a jack' do
-			lower = Card.new(suit: :spades, rank: :jack)
-			higher = Card.new(suit: :spades, rank: :queen)
+			lower = card(rank: :jack)
+			higher = card(rank: :queen)
 
 			raise unless higher.rank > lower.rank
 		end
@@ -29,8 +88,8 @@ RSpec.describe 'a playing card' do
 
 	describe 'a king' do
 		it 'ranks higher than a queen' do
-			lower = Card.new(suit: :spades, rank: :queen)
-			higher = Card.new(suit: :spades, rank: :king)
+			lower = card(rank: :queen)
+			higher = card(rank: :king)
 
 			raise unless higher.rank > lower.rank
 		end
